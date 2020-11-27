be4024d94be96b941e30570204b07266001048ca
SolievOliver <68802261+SolievOliver@users.noreply.github.com>
2020-11-27 18:57:33 +0500

    Homework 3

--- app/src/main/java/com/oliverworks/myapp/FragmentMoviesList.kt
+++ app/src/main/java/com/oliverworks/myapp/FragmentMoviesList.kt
@@ -1,12 +1,16 @@
 package com.oliverworks.myapp
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 
 class FragmentMoviesList : Fragment() {
+    private var listener:GoToDetailsListener? = null
+    lateinit var constraintLayout: ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,7 +19,27 @@ class FragmentMoviesList : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        constraintLayout = view.findViewById(R.id.mainBody)
+        constraintLayout.setOnClickListener{
+            goToDetails()
+        }
+        return view;
     }
 
+    private fun goToDetails() {
+        listener?.goToDetails()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is GoToDetailsListener){
+            listener = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
 }
--- app/src/main/java/com/oliverworks/myapp/MainActivity.kt
+++ app/src/main/java/com/oliverworks/myapp/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.oliverworks.myapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),GoToDetailsListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction()
             .apply {
-                add(R.id.main_container,FragmentMoviesDetails())
+                add(R.id.listContainer,FragmentMoviesList())
                 commit()
             }
     }
+
+    override fun goToDetails() {
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.mainActivityBackground,FragmentMoviesDetails())
+            addToBackStack(null)
+            commit()
+        }
+    }
+
 }
